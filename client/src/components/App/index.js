2a669d317656ddd0daeaa0c975e64094f9d84bfa
Gavneet <gavneet.bhandal@gmail.com>
2024-02-13 19:32:11 -0500

    added navigation bar, added routes, fixed folder directories

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import Review from './Review';
+import * as React from 'react';
+import Navigation from '../Navigation/Navigation';
+import Landing from '../Landing';
+import Matching from '../Matching';
+import Calendar from '../Calendar';
+import Resources from '../Resources';
 
-function App() {
-  return <Review />;
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/matching" element={<Matching />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/resources" element={<Resources />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
