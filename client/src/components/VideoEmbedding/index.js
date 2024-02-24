9be3a45bfae0b8413ba84d3cc47e8f8d9cf8d0b4
dhruvsharma11 <93293451+dhruvsharma11@users.noreply.github.com>
2024-02-23 22:25:55 -0500

    Added Profile Questions

--- client/src/components/VideoEmbedding/index.js
+++ client/src/components/VideoEmbedding/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import {JitsiMeeting} from '@jitsi/react-sdk';
+import VideoCall from './VideoCall';
 
 export default function VideoEmbedding() {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
-      <JitsiMeeting
-        // domain = { YOUR_DOMAIN }
-        roomName="PleaseUseAGoodRoomName"
-        configOverwrite={{
-          startWithAudioMuted: true,
-          disableModeratorIndicator: true,
-          startScreenSharing: true,
-          enableEmailInStats: false,
-        }}
-        interfaceConfigOverwrite={{
-          DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
-        }}
-        userInfo={{
-          displayName: 'YOUR_USERNAME',
-        }}
-        onApiReady={externalApi => {
-          // here you can attach custom event listeners to the Jitsi Meet External API
-          // you can also store it locally to execute commands
-        }}
-        getIFrameRef={iframeRef => {
-          iframeRef.style.height = '800px';
-        }}
-      />
+      <VideoCall />
     </div>
   );
 }
