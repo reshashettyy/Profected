d07715069f223978ee20513b136f2604b9062641
dhruvsharma11 <93293451+dhruvsharma11@users.noreply.github.com>
2024-03-13 22:56:31 -0400

    Added University Selection and Submit Button

--- client/src/components/Matching/index.js
+++ client/src/components/Matching/index.js
@@ -4,9 +4,12 @@ import Box from '@mui/material/Box';
 import ProgramSelection from './ProgramSelection';
 import CareerSelection from './CareerSelection';
 import TimeDate from './TimeDate';
+import UniversitySelection from './UniversitySelection';
+import {Button} from '@mui/material';
 
 function App() {
   const [selectedProgram, setSelectedProgram] = useState(null);
+  const [selectedUniversity, setSelectedUniversity] = useState(null);
   const [selectedInterest, setSelectedInterest] = useState('');
 
   const handleProgramChange = program => {
@@ -15,6 +18,16 @@ function App() {
 
   const handleInterestChange = interest => {
     setSelectedInterest(interest);
+  };
+
+  const handleUniversityChange = university => {
+    setSelectedUniversity(university);
+  };
+
+  const handleSubmit = () => {
+    console.log('University:', selectedUniversity);
+    console.log('Program:', selectedProgram);
+    console.log('Interest:', selectedInterest);
   };
 
   return (
@@ -27,6 +40,12 @@ function App() {
     >
       <Grid container spacing={2} style={{maxWidth: 400}}>
         <Grid item xs={12}>
+          <Box>
+            <UniversitySelection
+              selectedUniversity={selectedUniversity}
+              handleUniversityChange={handleUniversityChange}
+            />
+          </Box>
           <Box pb={2}>
             <ProgramSelection
               selectedProgram={selectedProgram}
@@ -42,6 +61,18 @@ function App() {
         </Grid>
         <Grid item xs={12}>
           <TimeDate />
+        </Grid>
+        <Grid item xs={12}>
+          {' '}
+          {/* This ensures the button aligns properly under the TimeDate component */}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            fullWidth
+          >
+            Submit
+          </Button>
         </Grid>
       </Grid>
     </Box>
